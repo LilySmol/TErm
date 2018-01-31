a78985d5a8938a59c1b001f76712ee1fa36eb792
NewUser <NewUser>
2018-02-01 00:00:34 +0400

    basePrototipe

---
+++ Term/Controllers/ProjectsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
+using System.Collections.Specialized;
+
+namespace Term.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private string host = "http://127.0.0.1/redmine/";
+        private string apiKey = "";
+        private List<string> projectList = new List<string>();
+
+        public ActionResult Projects()
+        {
+            var manager = new RedmineManager(host, apiKey);
+            var parameters = new NameValueCollection { { RedmineKeys.STATUS_ID, RedmineKeys.ALL } };
+            foreach (var issue in manager.GetObjects<Issue>(parameters))
+            {
+                projectList.Add(issue.Project.Name);
+            }
+            ViewBag.Entries = projectList;
+            return View();
+        }
+    }
+}
---
+++ Term/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Term.Startup))]
+namespace Term
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
